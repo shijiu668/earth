ae2ff2cd7fc3880f01287685e4aa753dab7a6c5e
shijiu <shijiu8861@gmail.com>
2025-07-11 00:03:21 +0800

    one2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { AuthProvider } from '../components/AuthProvider';
+import { AuthProvider } from './components/AuthProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
