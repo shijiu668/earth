98f6e7caf5c1d0e7f863fcd94a3bc7a13b650e13
shijiu <shijiu8861@gmail.com>
2025-07-11 12:49:47 +0800

    one20

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -68,7 +68,7 @@ export default function Footer() {
             <ul className="space-y-3">
               <li>
                 <a href="#" className="text-gray-400 hover:text-white transition-colors">
-                  Help Center2
+                  Help Center3
                 </a>
               </li>
               <li>
